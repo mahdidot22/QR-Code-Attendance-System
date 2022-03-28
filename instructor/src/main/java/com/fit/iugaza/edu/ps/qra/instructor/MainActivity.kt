c8a9ce3086a98d16ed2e0b9e89e6b5b3cbabb23f
Mahdi Taha <mtaha@students.iugaza.edu.ps>
2022-03-28 17:51:26 +0300

    mdot get startted

---
+++ instructor/src/main/java/com/fit/iugaza/edu/ps/qra/instructor/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fit.iugaza.edu.ps.qra.instructor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
